8be2f3ff9cb893ac26e87ae35ec41b030a0f5366
Richmond Watkins <richmondwatkins@mac.com>
2014-10-16 15:16:35 -0500

    Initial Commit

---
+++ FavoritePhotosChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FavoritePhotosChallenge
+//
+//  Created by Richmond on 10/16/14.
+//  Copyright (c) 2014 Richmond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
